19f542b192d86b1839f6e39ab865a0ad2ad65eb5
devussy <seungyong.yun@styleshare.kr>
2021-05-25 05:09:28 +0900

    Initial Commit

---
+++ app/src/main/java/fastcampus/aop/part5/chapter07/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package fastcampus.aop.part5.chapter07.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/fastcampus/aop/part5/chapter07/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package fastcampus.aop.part5.chapter07.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
